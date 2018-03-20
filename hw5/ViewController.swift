86f971245deac752e048ab98000d7a48be217b74
Don Ostergaard <oster012@rangers.uwp.edu>
2018-03-20 16:31:05 -0500

    Initial Commit

---
+++ hw5/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  hw5
+//
+//  Created by Don Ostergaard on 3/20/18.
+//  Copyright © 2018 Don Ostergaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet weak var Root: UIStackView!
+    @IBOutlet var SuperView: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
